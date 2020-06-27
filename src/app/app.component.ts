836302f84f23c15ce0c645d8b360c9e9a4e83450
ZangiefWins <fakesupremo2015@hotmail.com>
2020-06-27 16:39:03 -0300

    Finished app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'MatrixBin2Dec';
+export class AppComponent implements OnInit {
+
+  buttonClicked: boolean;
+  binaryNumber: string;
+  decimalNumber: string;
+  lastEditedInput: number;
+
+  constructor(private _clipboardService: ClipboardService){
+    
+  }
+
+  ngOnInit(): void {
+    this.binaryNumber = "";
+    this.decimalNumber = "";
+  }
+
+  click() {
+    if (this.assertValidity()) {
+      this.buttonClicked = true;
+
+      setTimeout(() => {
+        this.buttonClicked = false;
+      }, 400);
+
+      if (this.lastEditedInput == 1 && this.binaryNumber) {
+        this.decimalNumber = parseInt(this.binaryNumber, 2).toString();
+        this.copy(this.decimalNumber);
+      } else if (this.lastEditedInput == 2 && this.decimalNumber) {
+        this.binaryNumber = parseInt(this.decimalNumber).toString(2);
+        this.copy(this.binaryNumber);
+      }
+    }
+  }
+
+  userInput(origin: number): void {
+    if (origin == 1) {
+      setTimeout(() => {
+        this.binaryNumber = this.binaryNumber.replace(/[^0-1]/g, '');
+      }, 1);
+    } else {
+      setTimeout(() => {
+        this.decimalNumber = this.decimalNumber.replace(/[^0-9]/g, '');
+      }, 1);
+    }
+
+    this.lastEditedInput = origin;
+  }
+
+  copy(text: string){
+    this._clipboardService.copyFromContent(text)
+  }
+
+  assertValidity(): boolean {
+    return this.lastEditedInput == 1 && this.binaryNumber != "" || this.lastEditedInput == 2 && this.decimalNumber != "";
+  }
+
+  maskString() {
+    return '0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9';
+  }
+
 }
